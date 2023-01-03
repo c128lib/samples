ac9f6143e660998631689bdfd2098aed3a3c8d10
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2023-01-03 22:11:09 +0100

    Added cia.asm

---
+++ cia.asm
@@ -0,0 +1,59 @@
+#importonce 
+.segmentdef Cia [start=$1c01]
+.segment Cia
+
+c128lib_BasicUpstart128($1c10)
+
+* = $1c10 "Entry"
+Entry: {
+  // Clean screen
+    jsr c128lib.Kernal.CINT
+
+// Set cursor on (0, 5)
+    clc
+    ldx #5
+    ldy #0
+    jsr $fff0
+
+// Print string at current cursor position
+    ldx #0
+  !:
+    lda Joy1, x
+    jsr $FFD2
+    inx
+    cpx #$1B
+    bne !-
+
+// Set cursor on (0, 6)
+    clc
+    ldx #6
+    ldy #0
+    jsr $fff0
+
+// Print string at current cursor position
+    ldx #0
+  !:
+    lda Joy2, x
+    jsr $FFD2
+    inx
+    cpx #$1B
+    bne !-
+
+  Loop:
+    c128lib_GetFirePressedPort1()
+    sta $04E2
+    beq !Zero+
+    sta 
+
+    c128lib_GetFirePressedPort2()
+    sta $050B
+    
+    jmp Loop
+}
+
+Joy1: .text "JOYSTICK 1 (0 IS PRESSED): "
+Joy2: .text "JOYSTICK 2 (0 IS PRESSED): "
+
+#import "./common/lib/common-global.asm"
+#import "./common/lib/kernal.asm"
+#import "./chipset/lib/cia-global.asm"
