b13ea956a42521ddaa400fb33a6cc84eb23e9290
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2023-01-04 15:31:18 +0100

    Better using labels and macros on cia

--- cia.asm
+++ cia.asm
@@ -13,61 +13,61 @@ Entry: {
     clc
     ldx #5
     ldy #0
-    jsr $fff0
+    jsr c128lib.Kernal.PLOT
 
 // Print string at current cursor position
     ldx #0
   !:
     lda Joy1, x
-    jsr $FFD2
+    jsr c128lib.Kernal.BSOUT
     inx
-    cpx #$1B
+    cpx #Joy1Length
     bne !-
 
 // Set cursor on (0, 6)
     clc
     ldx #6
     ldy #0
-    jsr $fff0
+    jsr c128lib.Kernal.PLOT
 
 // Print string at current cursor position
     ldx #0
   !:
     lda Joy2, x
-    jsr $FFD2
+    jsr c128lib.Kernal.BSOUT
     inx
-    cpx #$1B
+    cpx #Joy2Length
     bne !-
 
   Loop:
     c128lib_GetFirePressedPort1()
-    clc
-    beq !Zero+
-    adc #33
-    jmp !Print+
-  !Zero:
-    adc #48
-
-  !Print:
-    sta $04E3
+    ConvertAccumulatorInZeroOrOneChar()
+    sta $400 + c128lib_getTextOffset(27, 5)
 
     c128lib_GetFirePressedPort2()
-    clc
-    beq !Zero+
-    adc #33
-    jmp !Print+
-  !Zero:
-    adc #48
-
-  !Print:
-    sta $050B
+    ConvertAccumulatorInZeroOrOneChar()
+    sta $400 + c128lib_getTextOffset(27, 6)
     
     jmp Loop
 }
 
 Joy1: .text "JOYSTICK 1 (0 IS PRESSED): "
+.label Joy1Length = $1B;
 Joy2: .text "JOYSTICK 2 (0 IS PRESSED): "
+.label Joy2Length = $1B;
+
+.macro ConvertAccumulatorInZeroOrOneChar() {
+    clc
+    beq !Zero+
+    adc #33
+    jmp !Print+
+  !Zero:
+    adc #48
+
+  !Print:
+}
 
 #import "./common/lib/common-global.asm"
 #import "./common/lib/kernal.asm"
 #import "./chipset/lib/cia-global.asm"
+#import "./chipset/lib/vic2-global.asm"
